afc179d4c7e098cec3c7f274549a81f20b41f91d
Italo Paiva <italosensei@hotmai.com>
2014-09-13 15:36:02 -0300

    First commit

---
+++ peNaEstrada/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeNaEstrada::Application.config.secret_key_base = 'a52c15823e71acb2580ecc28f274c9412d7f6218249f00d3bfa486e83f8540acf1537d2cd809c89a211a146716f7d174dafaf30d044c4b5ed95cec7f39304e67'
---
+++ peNaEstrada/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PeNaEstrada::Application.config.session_store :cookie_store, key: '_peNaEstrada_session'
